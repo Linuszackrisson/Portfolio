4d69bbe313fe6dcfd7b80bdc63b163b7d2a256b7
Linuszackrisson <143511885+Linuszackrisson@users.noreply.github.com>
2024-01-31 11:37:10 +0100

    More global tweaks

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Hantera klick på länkar
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+  
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
